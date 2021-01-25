65631101c12b60997ebcdacc718d354c245e970e
liucongxin1 <yaoxuan@airdoc.com>
2021-01-25 16:24:07 +0800

    init proj

---
+++ echo-server.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import socket
+
+HOST = "172.16.0.34"  # Standard loopback interface address (localhost)
+PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    print("server listening on host: %s, port: %s" % (HOST, PORT))
+    conn, addr = s.accept()
+    with conn:
+        print("Connected by", addr)
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            conn.sendall(data)
